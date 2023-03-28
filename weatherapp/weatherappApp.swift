211456c30b593f4d7e3e6765e6c7b0bd8abbd46f
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-03-27 21:55:01 -0300

    initial commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Anderson Vanzo on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
